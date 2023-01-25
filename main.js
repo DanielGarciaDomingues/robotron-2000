339917f8fae99e33a7e7584f1d52d226422aa09b
Daniel <daniel@pop-os.localdomain>
2023-01-25 12:20:18 -0300

    Projeto finalizado: operadores funcionais e otimizacao com funcao de verificacao de sinal.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const controles = document.querySelectorAll("[data-ajuste]")
-// const ajustes = document.querySelectorAll("[data-ajuste]")
-// const contador = document.querySelectorAll('.controle-contador')
+const estatistica = document.querySelectorAll("[data-estatistica]")
+const peca = document.querySelectorAll("[data-pecas]")
 const pecas = {
     "bracos": {
         "forca": 29,
@@ -35,17 +35,25 @@ const pecas = {
     }
 }
 function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector("[data-contador]")
-    // console.log(operacao)
-    if (operacao === "+"){
-        peca.value = parseInt(peca.value) + 1
-    } else {
-        peca.value = parseInt(peca.value) - 1
-    }
+    const contador = controle.querySelector("[data-contador]")
+    contador.value = parseInt(contador.value) + verificaSinal(operacao, 1)
+    
+}
+function atualizaEstatisticas (operador, peca) {
+    
+    estatistica.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent ) + verificaSinal(operador, pecas[peca][elemento.dataset.estatistica])
+    })         
+}
+function verificaSinal(sinal, elemento){
+    if(sinal === "+"){
+        return elemento
+    }else{return -1*elemento}
 }
 
 controles.forEach((elemento) => { 
     elemento.addEventListener("click" , (evento) => {
         manipulaDados(evento.target.dataset.ajuste, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.ajuste, evento.target.dataset.pecas)
     })}
 )
